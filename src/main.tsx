244282967bcde2c90ee9139a6be8d92c9e592d16
nilserratio <nilserratio@gmail.com>
2023-05-29 18:34:55 +0200

    Create theme with principal colors and font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import "@fontsource/poppins";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
