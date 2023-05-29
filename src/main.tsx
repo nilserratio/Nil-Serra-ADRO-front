f7abd3c1c46e0b51525d502bae1792acaa45bc7a
nilserratio <nilserratio@gmail.com>
2023-05-29 19:08:34 +0200

    Create GlobalStyle and reset styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,17 @@ import "@fontsource/poppins";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme/theme";
+import GlobalStyle from "./styles/GlobalStyles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
