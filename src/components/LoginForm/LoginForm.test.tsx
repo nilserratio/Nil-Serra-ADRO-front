ac2b82f77441366eedf436edc1ed4d5ebd28948a
nilserratio <nilserratio@gmail.com>
2023-05-30 20:45:17 +0200

    Add to LoginForm component onSubmitUserData functionality

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,14 +1,17 @@
 import { screen } from "@testing-library/react";
+import { vi } from "vitest";
 import LoginForm from "./LoginForm";
 import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a LoginForm component", () => {
   describe("When it's rendered", () => {
+    const mockOnSubmit = vi.fn();
+
     test("Then it should show two labels with a placeholder text 'Username' and 'Password'", () => {
       const usernameText = "Username";
       const passwordText = "Password";
 
-      renderWithProviders(<LoginForm />);
+      renderWithProviders(<LoginForm actionOnSubmit={mockOnSubmit} />);
 
       const usernameLabelText = screen.getByLabelText(usernameText);
       const passwordLabelText = screen.getByLabelText(passwordText);
@@ -16,15 +19,15 @@ describe("Given a LoginForm component", () => {
       expect(usernameLabelText).toBeInTheDocument();
       expect(passwordLabelText).toBeInTheDocument();
     });
-  });
 
-  test("Then it should show a button with the test 'Sign in'", () => {
-    const signInText = "Sign in";
+    test("Then it should show a button with the test 'Sign in'", () => {
+      const signInText = "Sign in";
 
-    renderWithProviders(<LoginForm />);
+      renderWithProviders(<LoginForm actionOnSubmit={mockOnSubmit} />);
 
-    const signInButton = screen.getByRole("button", { name: signInText });
+      const signInButton = screen.getByRole("button", { name: signInText });
 
-    expect(signInButton).toBeInTheDocument();
+      expect(signInButton).toBeInTheDocument();
+    });
   });
 });
