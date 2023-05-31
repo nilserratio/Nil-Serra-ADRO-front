a3a0d23ebdbb66dce41d542e02c7d4bde66f6fc5
nilserratio <nilserratio@gmail.com>
2023-05-31 12:54:36 +0200

    Add decodeToken function test

--- src/mocks/user/userMocks.ts
+++ src/mocks/user/userMocks.ts
@@ -2,8 +2,9 @@ import { UserStateStructure, UserTokenStructure } from "../../store/types";
 import { UserCredentials } from "../../types";
 
 export const userTokenDataMock: UserTokenStructure = {
-  id: "646fa090b926156009746913",
-  token: "mocked-token",
+  id: "646a4a6ae27e102276f098d6",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDZhNGE2YWUyN2UxMDIyNzZmMDk4ZDYiLCJuYW1lIjoidXNhaWFzIiwiaWF0IjoxNjg0NzQ5ODIwLCJleHAiOjE2ODUzNTQ2MjB9.oPNY8Bunrz4W6e540NRbHyL1qEbJUbha5a-_BGo-fX4",
 };
 
 export const userInitialStateMock: UserStateStructure = {
