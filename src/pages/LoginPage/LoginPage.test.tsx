5349aed31f3e01ec6f247f52122e03a23825a537
nilserratio <nilserratio@gmail.com>
2023-06-01 17:36:48 +0200

    Add loginOnSubmit sign in test on LoginPage component

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,9 +1,16 @@
 import LoginPage from "./LoginPage";
 import { screen } from "@testing-library/react";
 import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import {
   renderWithProviders,
   wrapWithRouter,
 } from "../../utils/testUtils/testUtils";
+import { paths } from "../../utils/paths/paths";
 
 describe("Given a LoginPage component", () => {
   describe("When rendered", () => {
@@ -17,6 +24,37 @@ describe("Given a LoginPage component", () => {
       });
 
       expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user clicks the 'Sign in' button", () => {
+    test("Then it should log in the user and redirects him to the '/home' path", async () => {
+      const usernameText = "Username";
+      const passwordText = "Password";
+      const usernameInputText = "John";
+      const passwordInputText = "John";
+      const buttonText = "Sign in";
+
+      const route: RouteObject[] = [
+        {
+          path: "/",
+          element: <LoginPage />,
+        },
+      ];
+
+      const router = createMemoryRouter(route);
+
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const usernameTextField = screen.getByLabelText(usernameText);
+      const passwordTextField = screen.getByLabelText(passwordText);
+      const loginButton = screen.getByRole("button", { name: buttonText });
+
+      await userEvent.type(usernameTextField, usernameInputText);
+      await userEvent.type(passwordTextField, passwordInputText);
+      await userEvent.click(loginButton);
+
+      expect(router.state.location.pathname).toStrictEqual(paths.home);
     });
   });
 });
