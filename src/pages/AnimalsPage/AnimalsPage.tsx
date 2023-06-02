01a575af0dd402fdb0f6e385a7e22176f79bef0a
nilserratio <nilserratio@gmail.com>
2023-06-02 15:55:12 +0200

    Create AnimalsPage page component

---
+++ src/pages/AnimalsPage/AnimalsPage.tsx
@@ -0,0 +1,11 @@
+import AnimalsPageStyled from "./AnimalsPageStyled";
+
+const AnimalsPage = (): React.ReactElement => {
+  return (
+    <AnimalsPageStyled className="animals-container">
+      <h1 className="animals-container__title">In adoption</h1>
+    </AnimalsPageStyled>
+  );
+};
+
+export default AnimalsPage;
