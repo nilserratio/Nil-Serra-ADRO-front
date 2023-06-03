135152691d769e77f5ef2b09ad1051aa3c89bfb8
nilserratio <nilserratio@gmail.com>
2023-06-03 16:32:31 +0200

    Create AnimalsListStyled styles

---
+++ src/components/AnimalsList/AnimalsListStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AnimalsListStyled = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 28px;
+`;
+
+export default AnimalsListStyled;
