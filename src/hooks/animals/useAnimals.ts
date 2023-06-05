e715672ecbf55103421c2d9d09c5124abe7b25e1
nilserratio <nilserratio@gmail.com>
2023-06-05 10:15:01 +0200

    Create getAnimals fucntion on useAnimals custom hook

---
+++ src/hooks/animals/useAnimals.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { paths } from "../../utils/paths/paths";
+import { AnimalDataStructure } from "../../types";
+import { useCallback } from "react";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const useAnimals = () => {
+  const getAnimals = useCallback(async (): Promise<AnimalDataStructure[]> => {
+    const {
+      data: { animals },
+    } = await axios.get<{ animals: AnimalDataStructure[] }>(
+      `${apiUrl}${paths.home}`
+    );
+
+    return animals;
+  }, []);
+
+  return { getAnimals };
+};
+
+export default useAnimals;
