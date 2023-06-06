b98e03c6a41361e13b996aaf7ddf1eeb23d367ee
nilserratio <nilserratio@gmail.com>
2023-06-06 15:54:55 +0200

    Add error management to useAnimals custom hook test

--- src/hooks/animals/__test__/getAnimals.test.ts
+++ src/hooks/animals/__test__/getAnimals.test.ts
@@ -2,6 +2,8 @@ import { renderHook } from "@testing-library/react";
 import useAnimals from "../useAnimals";
 import { animalsMock } from "../../../mocks/animals/animalsMocks";
 import { wrapper } from "../../../utils/testUtils/testUtils";
+import { server } from "../../../mocks/server";
+import { errorHandlers } from "../../../mocks/handlers";
 
 describe("Given a getAnimals fucntion", () => {
   describe("When it's called", () => {
@@ -17,6 +19,22 @@ describe("Given a getAnimals fucntion", () => {
       const expectedAnimals = animalsMock;
 
       expect(animals).toStrictEqual(expectedAnimals);
+    });
+  });
+
+  describe("When it's called but can't connet to the Api Rest", () => {
+    test("Then it should show a feedback modal error with the message 'Something went wrong. Try it again please.'", async () => {
+      server.resetHandlers(...errorHandlers);
+
+      const {
+        result: {
+          current: { getAnimals },
+        },
+      } = renderHook(() => useAnimals(), { wrapper: wrapper });
+
+      const animals = await getAnimals();
+
+      expect(animals).toBeUndefined();
     });
   });
 });
