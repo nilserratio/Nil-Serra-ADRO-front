151239730d58e4aec89d46a45d24893e073b1245
nilserratio <nilserratio@gmail.com>
2023-06-06 12:38:41 +0200

    Add showFeedback reducer test

---
+++ src/store/iu/__tests__/showFeedback.test.ts
@@ -0,0 +1,31 @@
+import { FeedbackPayloadStructure, UiStructure } from "../types";
+import { showFeedbackActionCreator, uiReducer } from "../uiSlice";
+
+describe("Given a showFeedback reducer", () => {
+  describe("When it recieve an Ui state and a show feedback action", () => {
+    test("Then it should toggle the showFeedback Ui state to true", () => {
+      const currentState: UiStructure = {
+        showFeedback: false,
+        isError: false,
+        message: "",
+      };
+
+      const expectedState: UiStructure = {
+        showFeedback: true,
+        isError: true,
+        message: "Test showFeedback error",
+      };
+
+      const payload: FeedbackPayloadStructure = {
+        isError: true,
+        message: "Test showFeedback error",
+      };
+      const UiState = uiReducer(
+        currentState,
+        showFeedbackActionCreator(payload)
+      );
+
+      expect(UiState).toStrictEqual(expectedState);
+    });
+  });
+});
