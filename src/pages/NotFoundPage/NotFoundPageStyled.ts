6dbe49a44a52741fa78b80562a44310e85e10cad
nilserratio <nilserratio@gmail.com>
2023-06-06 20:06:43 +0200

    Add NotFoundPageStyled styles

---
+++ src/pages/NotFoundPage/NotFoundPageStyled.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const NotFoundPageStyled = styled.main`
+  background-color: ${(props) => props.theme.colors.quatenary};
+  height: 100vh;
+
+  .notFound-container {
+    &__image {
+      position: relative;
+    }
+  }
+
+  .info-cotainer {
+    padding: 32px 22px;
+    position: absolute;
+    top: 105px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    gap: 10px;
+
+    &__text {
+      color: ${(props) => props.theme.colors.primary};
+      font-size: 2.5rem;
+      font-weight: bold;
+    }
+
+    &__title {
+      color: ${(props) => props.theme.colors.primary};
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+
+    &__button {
+      padding: 16px 32px;
+      margin-top: 10px;
+      border-radius: 12px;
+      background-color: ${(props) => props.theme.colors.secondary};
+      color: ${(props) => props.theme.colors.primary};
+      max-width: 400px;
+      width: 100%;
+    }
+  }
+`;
+
+export default NotFoundPageStyled;
