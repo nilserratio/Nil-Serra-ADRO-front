ad9807840693fe68b49ae5c692e650ef7a1998a5
nilserratio <nilserratio@gmail.com>
2023-06-06 12:45:29 +0200

    Add hideFeedback reducer test

--- src/store/iu/types.ts
+++ src/store/iu/types.ts
@@ -1,3 +1,11 @@
 export interface UiStructure {
-  isLoading: boolean;
+  isLoading?: boolean;
+  showFeedback?: boolean;
+  isError?: boolean;
+  message?: string;
+}
+
+export interface FeedbackPayloadStructure {
+  message: string;
+  isError: boolean;
 }
