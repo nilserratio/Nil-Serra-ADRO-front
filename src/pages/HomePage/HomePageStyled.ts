b228869fd523d3448da5dfd95153f05bb09edfac
Nil Serra Tió <nilserratio@gmail.com>
2023-11-03 12:23:06 +0100

    Add SliderAnimals styles on HomePage page

--- src/pages/HomePage/HomePageStyled.ts
+++ src/pages/HomePage/HomePageStyled.ts
@@ -20,6 +20,35 @@ const HomePageStyled = styled.main`
       font-size: 2rem;
       line-height: 48px;
     }
+
+    &__link {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 8px 24px;
+      width: 100%;
+      max-width: 276px;
+      border: solid;
+      border-color: ${(props) => props.theme.colors.secondaryBackground};
+      border-width: 1px;
+      border-radius: 12px;
+      background: ${(props) => props.theme.colors.primary};
+    }
+
+    &__feedback {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      gap: 20px;
+      padding: 40px 20px;
+      width: 100%;
+      max-width: 276px;
+      background-color: ${(props) => props.theme.colors.primary};
+      border-radius: 10px;
+      border: 1px solid rgb(217, 217, 217);
+    }
   }
 `;
 
